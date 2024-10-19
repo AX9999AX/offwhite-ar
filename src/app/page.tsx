e23ec092c59c60da9045ce393e0fe379b426b323
Volodymyr Cherkashyn <volo.cherkashyn@gmail.com>
2024-10-19 23:46:34 +0300

    Added QR and Footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -124,6 +124,18 @@ export default function Home() {
           </a>
         </div>
       </div>
+      <div className="my-10">
+        <Image
+          src="/img/qr.png"
+          alt="QR"
+          width={500}
+          height={500}
+          className="w-full h-auto"
+        />
+        <p className="mt-2 text-[24px] font-bold leading-[20px] text-center text-[#024BAD]">
+          EXample QR AR
+        </p>
+      </div>
     </>
   );
 }
